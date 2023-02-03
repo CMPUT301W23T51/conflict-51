f207a723d80868a0fa3214020c89ac18f042da91
prabhjo1 <prabhjo1@ualberta.ca>
2023-02-02 19:59:35 -0700

    Lab 4 base code

---
+++ conflict51/app/src/main/java/com/example/conflict_51/Shape.java
@@ -0,0 +1,14 @@
+package com.example.conflict_51;
+
+public abstract class Shape {
+    protected int x;
+    protected int y;
+    public Shape() {
+
+    }
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
